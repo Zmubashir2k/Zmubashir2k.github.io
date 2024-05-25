3b24b4a800098d562c5617e784ea3a3462361ad3
Zain Mubashir <98364780+Zmubashir2k@users.noreply.github.com>
2024-05-25 15:15:21 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
-// This is where you can add any JavaScript you might need for your portfolio
+// Initialize WOW.js
+new WOW().init();
+
+// Smooth scrolling for anchor links
+$(document).ready(function(){
+    $('a[href*="#"]').on('click', function(e) {
+        e.preventDefault();
+
+        $('html, body').animate({
+            scrollTop: $($(this).attr('href')).offset().top
+        }, 500, 'linear');
+    });
+});
+
 console.log("Welcome to my portfolio!");
