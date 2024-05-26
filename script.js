219576c8d43493bc09620ca34591730664152af7
Zain Mubashir <98364780+Zmubashir2k@users.noreply.github.com>
2024-05-26 12:31:03 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
-// This is where you can add any JavaScript you might need for your portfolio
-console.log("Welcome to my portfolio!");
+document.getElementById('theme-toggle').addEventListener('click', function() {
+    const root = document.documentElement;
+    if (root.getAttribute('data-theme') === 'light') {
+        root.setAttribute('data-theme', 'dark');
+    } else {
+        root.setAttribute('data-theme', 'light');
+    }
+});
